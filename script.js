bc3448733f6e2ec57209e742178b23b57fc7ddfe
Sakshi Mandvakar <sakshimandvakar6094@gmail.com>
2024-09-11 11:18:01 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const fileInput = document.querySelector(".file-input"),
+filterOptions = document.querySelectorAll(".filter button");
 previewImg = document.querySelector(".preview-img img");
 chooseImgBtn = document.querySelector(".choose-img");
+
 
 
 const loadImage = () =>{
     let file = fileInput.files[0];
     if(!file) return;
     previewImg.src = URL.createObjectURL(file);
+    previewImg.addEventListener("load",() => {
+        document.querySelector(".container").classList.remove("disable");
+    })
     
 }
+filterOptions.forEach(option => {
+    option.addEventListener("click",() =>{
+        document.querySelector(".filter .active").classList.remove("active");
+        option.classList.add("active");
+    });
+});
 
 fileInput.addEventListener("change", loadImage);
 
